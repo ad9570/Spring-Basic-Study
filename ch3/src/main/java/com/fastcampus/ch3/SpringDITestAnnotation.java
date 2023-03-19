6e686eb771e0003ee1c9eadce032cef84fe9a13f
MinWooPark <narris64@gmail.com>
2023-03-19 11:17:09 +0900

    SpringDI

---
+++ ch3/src/main/java/com/fastcampus/ch3/SpringDITestAnnotation.java
@@ -0,0 +1,76 @@
+package com.fastcampus.ch3;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+class Computer {
+    @Autowired                  // byType 먼저 검색 -> Type 중복 시 byName 검색(기본 이름인 "타입의 첫글자 소문자")
+    Core core;
+    /*
+    @Autowired                  // Type 중복 + 기본 이름 존재하지 않음 + 다른 이름 여러 개 존재 시 Autowire 불가능(에러)
+    @Qualifier("windowsOS")     // "@Qualifier로 검색할 이름 지정"
+     */
+    @Resource(name = "macOS")   // 혹은 @Resource 사용 : byName 먼저 검색
+    OperatingSystem os;
+    @Value("32")                // 기본형, String : @Value로 값 주입, 기본형은 기본형으로 자동 변환
+    int memory;
+    @Autowired                  // @Autowired는 배열이나 컬렉션 대상으로도 타입에 맞는 1개의 객체만 주입 가능
+    List<Storage> storage;
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "core=" + core +
+                ", os=" + os +
+                ", memory=" + memory +
+                ", storage=" + storage +
+                '}';
+    }
+}
+
+@Component("core")
+class Core {}                   // <bean id="core" class="com.fastcampus.ch3.Engine"/>
+@Component("intel")
+class Intel extends Core {}
+@Component("amd")
+class Amd extends Core {}
+
+class OperatingSystem {}
+@Component("windowsOS")
+class WindowsOS extends OperatingSystem {}
+@Component("macOS")
+class MacOS extends OperatingSystem {}
+
+@Component
+class Storage {}
+
+public class SpringDITestAnnotation {
+    public static void main(String[] args) {
+        ApplicationContext ac = new GenericXmlApplicationContext("config-component.xml");
+        Computer computer = ac.getBean(Computer.class);
+
+        System.out.println("computer = " + computer + "\n");
+
+        try {
+            Core core = (Core) ac.getBean("core");  // byName - 상속 등으로 같은 타입이 여러 개 존재 시 byName 사용
+            System.out.println("core = " + core);
+        } catch (BeansException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Core core = ac.getBean(Core.class);     // byType - 상속 등으로 같은 타입이 여러 개 존재 시 ERROR
+            System.out.println("core = " + core);
+        } catch (BeansException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ ch3/src/main/java/com/fastcampus/ch3/SpringDITestBean.java
@@ -0,0 +1,97 @@
+package com.fastcampus.ch3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Arrays;
+
+class Car {
+    String color;
+    String oil;
+    Engine engine;
+    Door[] doors;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setOil(String oil) {
+        this.oil = oil;
+    }
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+    public void setDoors(Door[] doors) {
+        this.doors = doors;
+    }
+
+    public Car() {
+        this("white", "electric", new Engine(), new Door[]{new Door(), new Door()});
+    }
+    public Car(String color, String oil, Engine engine, Door[] doors) {
+        this.color = color;
+        this.oil = oil;
+        this.engine = engine;
+        this.doors = doors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", oil='" + oil + '\'' +
+                ", engine=" + engine +
+                ", doors=" + Arrays.toString(doors) +
+                '}';
+    }
+}
+
+class Sedan extends Car {
+    public Sedan() {
+        super();
+    }
+    public Sedan(String color, String oil, Engine engine, Door[] doors) {
+        super(color, oil, engine, doors);
+    }
+}
+
+class Bus extends Car {
+    public Bus() {
+        super();
+    }
+    public Bus(String color, String oil, Engine engine, Door[] doors) {
+        super(color, oil, engine, doors);
+    }
+}
+
+class Engine {}
+
+class Door {}
+
+public class SpringDITestBean {
+    public static void main(String[] args) {
+        ApplicationContext ac = new GenericXmlApplicationContext("config-bean.xml");
+        Car car = (Car) ac.getBean("car");          // byName
+
+        /* xml의 <bean> 태그로 대체 : 클래스 멤버의 Setter를 이용하므로 Setter 메서드 필수
+
+        Engine engine = ac.getBean(Engine.class);           // byType(형변환 불필요)
+        Engine engine2 = (Engine) ac.getBean("engine");     // getBean 할 때마다 같은 객체 반환(scope="singleton")
+        Door door1 = ac.getBean("door", Door.class);    // byName + byType(형변환 불필요)
+        Door door2 = ac.getBean(Door.class);           // getBean 할 때마다 새로운 객체 반환(scope="prototype")
+
+        car.setColor("gray");
+        car.setOil("gasolin");
+        car.setEngine(engine);
+        car.setDoors(new Door[]{door1, door2});
+
+         */
+
+        System.out.println("car = " + car);
+
+        Sedan sedan = ac.getBean("sedan", Sedan.class);
+        Bus bus = ac.getBean(Bus.class);
+
+        System.out.println("sedan = " + sedan);
+        System.out.println("bus = " + bus);
+    }
+}
