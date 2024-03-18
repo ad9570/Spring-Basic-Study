135998f9a656ed75b25068ce14b0df66a65e5629
MinWooPark <narris64@gmail.com>
2024-03-18 23:43:19 +0900

    response out stream

--- ch6/src/main/java/com/fastcampus/ch6/DateTeller.java
+++ ch6/src/main/java/com/fastcampus/ch6/DateTeller.java
@@ -4,20 +4,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
 // 년월일을 입력 시 요일을 반환하는 원격 프로그램
 @RestController
 public class DateTeller {
     @RequestMapping("/getDate")
-    public String getDate(HttpServletRequest request) {
+    public void getDate(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 1. 입력 - 전송받은 데이터
         String year = request.getParameter("year");
         String month = request.getParameter("month");
         String day = request.getParameter("day");
 
-        System.out.println("year = " + year);
-        System.out.println("month = " + month);
-        System.out.println("day = " + day);
+        // 2. 작업
+        // 2-1. 입력 텍스트를 정수로 변환
+        int yyyy = Integer.parseInt(year);
+        int mm = Integer.parseInt(month);
+        int dd = Integer.parseInt(day);
+        // 2-2. 요일 계산
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(yyyy, mm - 1, dd);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);  // 1 = 일요일, ... , 7 = 토요일
+        char date = "일월화수목금토".charAt(dayOfWeek - 1);
 
-        return null;
+        // 3. 출력 - 브라우저에 전송
+        response.setCharacterEncoding("ms949");
+        PrintWriter out = response.getWriter();
+        /*out.println("<html>");
+        out.println("<head></head>");
+        out.println("<body>");
+        out.println("year = " + year);
+        out.println("month = " + month);
+        out.println("day = " + day);
+        out.println("date = " + date);
+        out.println("</body>");
+        out.println("</html>");*/
+        out.println(
+                """
+                <html>
+                <head></head>
+                <body>
+                year = %s <br/>
+                month = %s <br/>
+                day = %s <br/>
+                date = %s
+                </body>
+                </html>
+                """.formatted(year, month, day, date)
+        );
     }
 }
